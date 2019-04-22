1b6aa8c5fe702e25b048625a14926f56c85fe08f
Jared Coleman <jaredraycoleman@gmail.com>
2019-04-22 11:05:06 -0700

    Adds pygcn stuff

---
+++ src/pygcn/pygcn/utils.py
@@ -0,0 +1,248 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import pathlib 
+import sys 
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+from scipy.sparse.linalg.eigen.arpack import eigsh
+
+scriptdir = pathlib.Path(__file__).resolve().parent
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+
+
+def sparse_to_tuple(sparse_mx):
+    """Convert sparse matrix to tuple representation."""
+    def to_tuple(mx):
+        if not sp.isspmatrix_coo(mx):
+            mx = mx.tocoo()
+        coords = np.vstack((mx.row, mx.col)).transpose()
+        values = mx.data
+        shape = mx.shape
+        return coords, values, shape
+
+    if isinstance(sparse_mx, list):
+        for i in range(len(sparse_mx)):
+            sparse_mx[i] = to_tuple(sparse_mx[i])
+    else:
+        sparse_mx = to_tuple(sparse_mx)
+
+    return sparse_mx
+
+def load_data_tf(dataset_str='cora'):
+    """
+    Loads input data from gcn/data directory
+
+    ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances
+        (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.y => the one-hot labels of the labeled training instances as numpy.ndarray object;
+    ind.dataset_str.ty => the one-hot labels of the test instances as numpy.ndarray object;
+    ind.dataset_str.ally => the labels for instances in ind.dataset_str.allx as numpy.ndarray object;
+    ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict
+        object;
+    ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
+
+    All objects above must be saved using python pickle module.
+
+    :param dataset_str: Dataset name
+    :return: All data input files loaded (as well the training/test data).
+    """
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        path = pathlib.Path(scriptdir, 'data', f'ind.{dataset_str}.{names[i]}')
+        with path.open('rb') as f: #open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file(str(pathlib.Path('data', f'ind.{dataset_str}.test.index')))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    # if dataset_str == 'citeseer':
+    #     # Fix citeseer dataset (there are some isolated nodes in the graph)
+    #     # Find isolated nodes, add them as zero-vecs into the right position
+    #     test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+    #     # tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+    #     # tx_extended[test_idx_range-min(test_idx_range), :] = tx
+    #     # tx = tx_extended
+    #     ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+    #     ty_extended[test_idx_range-min(test_idx_range), :] = ty
+    #     _ty = ty_extended
+
+    #     bads = np.where(~_ty.any(axis=1))[0]
+
+    #     for node, neighbors in graph.items():
+    #         for bad in bads:
+    #             if bad in neighbors:
+    #                 neighbors.remove(bad)
+
+    #     for bad in bads:
+    #         del graph[bad]
+
+    #     max_idx = tx.shape[0] + allx.shape[0]
+    #     test_idx_reorder = [e for e in test_idx_reorder if e < max_idx]    
+    #     test_idx_range = np.sort(test_idx_reorder)
+    
+    if dataset_str == 'citeseer':
+            # Fix citeseer dataset (there are some isolated nodes in the graph)
+            # Find isolated nodes, add them as zero-vecs into the right position
+            test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+            tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+            tx_extended[test_idx_range-min(test_idx_range), :] = tx
+            tx = tx_extended
+            ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+            ty_extended[test_idx_range-min(test_idx_range), :] = ty
+            ty = ty_extended
+    
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph)).tocoo()
+
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    labels = torch.LongTensor(np.where(labels)[1])
+    
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+
+    features = torch.FloatTensor(np.array(features.todense()))
+
+    idx_test = test_idx_range.tolist()
+    idx_train = range(len(y))
+    idx_val = range(len(y), len(y)+500)
+
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def load_data(dataset_str, preprocessed=True):
+    if preprocessed:
+        return load_data_tf(dataset_str)
+    else:
+        return load_data_pt(dataset_str)
+
+def load_data_pt(dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print(f'Loading {dataset} dataset...')
+
+    path = pathlib.Path(scriptdir, '..', 'data', dataset).resolve()
+
+    idx_features_labels = np.genfromtxt(path.joinpath(f'{dataset}.content'),
+                                        dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt(path.joinpath(f'{dataset}.cites'),
+                                    dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    idx_train = range(140)
+    idx_val = range(140, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    print(len(labels))
+    return correct / len(labels)
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+
+if __name__ == '__main__':
+    import pprint 
+    stuff = dict(zip(['adj', 'features', 'labels', 'idx_train', 'idx_val', 'idx_test'], load_data_tf('citeseer')))
+    
+    for key, item in stuff.items():
+        stuff[key] = (item.shape, item)
+
+    pprint.pprint(stuff)
+
+    import matplotlib.pyplot as plt 
+
+    elements = ['adj', 'features', 'labels', 'idx_train', 'idx_val', 'idx_test']
+    stuff = {'pt': {}, 'tf': {}}
+    for i, (tf, pt) in enumerate(zip(load_data_tf(), load_data_pt())):
+        stuff['pt'][elements[i]] = pt
+        stuff['tf'][elements[i]] = tf
+
+    g_pt = nx.from_numpy_matrix(stuff['pt']['adj'].to_dense().numpy())
+    g_tf = nx.from_numpy_matrix(stuff['tf']['adj'].to_dense().numpy())
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    nx.draw(g_pt, ax=ax1)
+    nx.draw(g_tf, ax=ax2)
+    plt.show()
